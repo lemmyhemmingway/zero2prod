5e9772bd7c3a355b34595776a185d6a39e53067b
Yasin <yasincaykur@gmail.com>
2024-02-13 21:41:58 +0300

    removing old version of the book

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-use std::net::TcpListener;
-
-use zerotoprod::{startup::run, configuration::get_configuration};
-
-#[actix_web::main] 
-async fn main() -> std::io::Result<()> {
-
-    let configuration = get_configuration().expect("Failed to read configuration");
-
-    let address = format!("127.0.0.1:{}", configuration.application_port);
-    
-    let listener = TcpListener::bind(address)?;
-
-    run(listener)?.await
+fn main() {
+    println!("Hello, world!");
 }
-
