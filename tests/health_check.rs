3b1b411fdf240841b407b6b76b0d32c364357a2f
Yasin <yasincaykur@gmail.com>
2024-02-26 23:48:39 +0300

    added port

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,16 @@
 use std::net::TcpListener;
 
 
+fn spawn_app() -> String{ 
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
+    let port = listener.local_addr().unwrap().port();
+    let server = zerotoprod::run(listener).expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+    dbg!("Running on {port}");
+
+    format!("127.0.0.1:{port}")
+}
 
 
 #[actix_web::test]
@@ -21,13 +31,50 @@ async fn health_check_works() {
     
 }
 
-fn spawn_app() -> String{ 
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
-    let port = listener.local_addr().unwrap().port();
-    let server = zerotoprod::run(listener).expect("Failed to bind address");
+#[actix_web::test]
+async fn subscribe_returns_a_200_for_a_valid_form_data() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
 
-    let _ = tokio::spawn(server);
-    dbg!("Running on {port}");
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+                    .post(&format!("{}/subscriptions", &address))
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .body(body)
+                    .send()
+                    .await
+                    .expect("Failed to execute request");
 
-    format!("127.0.0.1:{port}")
+
+    assert_eq!(200, response.status().as_u16())
+
 }
+
+#[actix_web::test]
+async fn subscribe_returns_400_when_data_is_missing() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email")
+    ];
+
+    // Act
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+                    .post(&format!("{}/subscriptions", &address))
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .body(body)
+                    .send()
+                    .await
+                    .expect("Failed to execute request");
+
+
+    assert_eq!(200, response.status().as_u16())
+
+}
+
