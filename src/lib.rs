b82976b8707e650c673bb3d16e4bc353e13b20c0
Yasin <yasincaykur@gmail.com>
2024-03-02 23:57:32 +0300

    added port

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,28 @@
-use std::net::TcpListener;
+use std::{hash::Hasher, net::TcpListener};
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder, dev::Server};
+use actix_web::{dev::Server, get, post, web, App, HttpResponse, HttpServer, Responder, Result};
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 #[get("/healthcheck")]
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[post("/subscriptions")]
+async fn subscribe(_form: web::Form<FormData>) -> Result<String> {
+    Ok(format!("{}", _form.name))
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .service(health_check)
+            .service(subscribe)
     })
     .listen(listener)?
     .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -7,9 +7,8 @@ fn spawn_app() -> String{
     let server = zerotoprod::run(listener).expect("Failed to bind address");
 
     let _ = tokio::spawn(server);
-    dbg!("Running on {port}");
 
-    format!("127.0.0.1:{port}")
+    format!("http://127.0.0.1:{}", port)
 }
 
 
@@ -20,7 +19,7 @@ async fn health_check_works() {
     let client = reqwest::Client::new();
 
     let response = client
-    .get(format!("http://{address}/healthcheck"))
+    .get(format!("{}/healthcheck", &address))
         .send()
         .await
     .expect("Failed to execute request");
@@ -64,17 +63,17 @@ async fn subscribe_returns_400_when_data_is_missing() {
     ];
 
     // Act
-    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-    let response = client
-                    .post(&format!("{}/subscriptions", &address))
-                    .header("Content-Type", "application/x-www-form-urlencoded")
-                    .body(body)
-                    .send()
-                    .await
-                    .expect("Failed to execute request");
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
 
-
-    assert_eq!(200, response.status().as_u16())
+        assert_eq!(400, response.status().as_u16(), "400 request when payload {}", error_message)
+    }
 
 }
 
